3109319a85ee19237730d3a7aefe207133222a9b
Aayushi Garg <gargayushi2003@gmail.com>
2023-07-14 12:02:48 +0530

    Router Setup

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const colors = require('colors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+//database connection function
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`Server running on ${mongoose.connection.host}`.bgCyan.white);
+    } catch (error) {
+        console.log(`${error}`.bgRed)
+    }
+}
+
+app.get('/', (req, res) => {
+    res.json({ msg: 'Hello' });
+});
+
+connectDb();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log("Server is running on port ", port);
+})
