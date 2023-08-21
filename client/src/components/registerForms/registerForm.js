7e26a30e97e71e36c804d6a8543498b054cbdfcc
Aayushi Garg <gargayushi2003@gmail.com>
2023-08-21 13:17:33 +0530

    Pages Setup

---
+++ client/src/components/registerForms/registerForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const registerForm = () => {
+    return (
+        <form>
+
+        </form>
+    )
+}
+
+export default registerForm
---
+++ client/src/pages/register.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './register.css'
+import registerImg from '../images/register.png'
+import logo from '../images/logoSocialSync.png'
+
+const register = () => {
+    return (
+        <div className='main-component'>
+            <div className="register--div">
+                <div className='register--image'>
+                    <div className="logo" style={{ height: '10px', width: '10px' }}>
+                        <img src={logo} />
+                    </div>
+                    <div className="image-r">
+                        <img src={registerImg} />
+                    </div>
+                </div>
+                <div className='register--form'>
+                    Create
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default register
