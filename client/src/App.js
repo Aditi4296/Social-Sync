8860c93493963b063b63c9b43f0455e2cac282c1
Aayushi Garg <gargayushi2003@gmail.com>
2023-10-02 02:03:55 +0530

    Post Creation Done

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import StatusModal from './components/StatusModal';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { refreshToken } from './redux/actions/authAction';
+import { getPosts } from './redux/actions/postAction';
 
 
 function App() {
@@ -21,6 +22,10 @@ function App() {
   useEffect(() => {
     dispatch(refreshToken())
   }, [dispatch])
+
+  useEffect(() => {
+    if (auth.token) dispatch(getPosts(auth.token))
+  }, [dispatch, auth.token])
 
   return (
     <div className="App">
