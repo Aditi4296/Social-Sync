df70f982f5cd53e5fcfc0364ae6cd2982fe61786
Aditi4296 <aditisingh4296@rediffmail.com>
2023-10-03 00:54:54 +0530

    comments on post

--- server/models/commentModel.js
+++ server/models/commentModel.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose')
 
 const commentSchema = new mongoose.Schema({
-   
+    content: {
+        type: String,
+        required: true
+    },
+    tag: Object,
+    reply: mongoose.Types.objectId,
+    likes: [{type: mongoose.Types.ObjectId, ref: 'user'}],
+    user: {type: mongoose.Types.ObjectId, ref: 'user'}
 }, {
-   timestamps: true
+    timestamps: true
 })
 
 module.exports = mongoose.model('comment', commentSchema)
