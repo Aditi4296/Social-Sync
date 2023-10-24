7ca632e32b5828ba7ed013934b82b4136f272eec
Aditi4296 <aditisingh4296@rediffmail.com>
2023-10-24 10:34:24 +0530

    discover posts

--- server/controllers/postCtrl.js
+++ server/controllers/postCtrl.js
@@ -1,5 +1,20 @@
 const Posts = require('../models/postModel')
 
+class APIfeatures {
+    constructor(query, queryString){
+        this.query = query;
+        this.queryString = queryString;
+    }
+
+    paginating(){
+        const page = this.queryString.page * 1 || 1
+        const limit = this.queryString.limit * 1 || 9
+        const skip = (page - 1) * limit
+        this.query = this.query.skip(skip).limit(limit)
+        return this;
+    }
+}
+
 const postCtrl = {
     createPost: async (req, res) => {
         try {
@@ -23,9 +38,11 @@ const postCtrl = {
     },
     getPosts: async (req, res) => {
         try {
-            const posts = await Posts.find({
+            const features =  new APIfeatures(Posts.find({
                 user: [...req.user.following, req.user._id]
-            }).sort('-createdAt')
+            }), req.query).paginating()
+
+            const posts = await features.query.sort('-createdAt')
                 .populate("user likes", "avatar username fullname")
                 .populate({
                     path: "comments",
@@ -97,7 +114,10 @@ const postCtrl = {
     },
     getUserPosts: async (req, res) => {
         try {
-            const posts = await Posts.find({user: req.params.id}).sort("-createdAt")
+            const features = new APIfeatures(Posts.find({user: req.params.id}), req.query)
+            .paginating()
+
+            const posts = await features.query.sort("-createdAt")
 
             res.json({
                 posts,
@@ -130,7 +150,42 @@ const postCtrl = {
             return res.status(500).json({msg: err.message})
         }
     },
-    
+    getPostsDicover: async (req, res) => {
+        try {
+
+            // const newArr = [...req.user.following, req.user._id]
+
+            // const num  = req.query.num || 9
+
+            // const posts = await Posts.aggregate([
+            //     { $match: { user : { $nin: newArr } } },
+            //     { $sample: { size: Number(num) } },
+            // ])
+
+            const features = new APIfeatures(Posts.find({
+                user: {$nin: [...req.user.following, req.user._id]}
+            }), req.query).paginating()
+
+            const posts = await features.query.sort('-createdAt')
+            .populate("user likes", "avatar username fullname")
+            .populate({
+                path: "comments",
+                populate: {
+                    path: "user likes",
+                    select: "-password"
+                }
+            })
+
+            res.json({
+                msg: 'Success!',
+                result: posts.length,
+                posts
+            })
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
 }
 
 module.exports = postCtrl
